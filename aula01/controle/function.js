6ba16afb6c6d83d770e3bbe6e32bdbf874217dfd
Luana Ferreira <85973402+efluana@users.noreply.github.com>
2022-03-08 12:15:45 -0300

    Add files via upload

---
+++ aula01/controle/function.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    $('.btn-send').click(function(e){
+
+        e.preventDefault();
+
+        let= dados = $ ('#form').serialize()
+
+        $('#retorno').empty()
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: dados,
+            url: '../modelo/retorno.php',
+            success: function(dados){
+                        $('#retorno').append(`
+                    <div class="col-12 col-sm-8 col-md-6">
+                    <div class="alert-primary">
+                        <h1 class="text-center text-dark">
+                            ${dados.mensagem}
+                        </h1>
+                    </div>
+                    <img src="../img/${dados.tipo}" class="img-fluid">
+                </div>
+                `)
+
+            }
+        })
+    })
+})
