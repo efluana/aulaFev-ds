ddd84ddbcbb156a09800e70707e16a97a72c7072
efluana <luanaferreira03.lf@gmail.com>
2022-03-15 12:10:12 -0300

    dnv

--- aula02/model/return.php
+++ aula02/model/return.php
@@ -1,14 +1,37 @@
 <?php
 
-    $NOME = $_REQUEST ['NOME'];
-    $BEBIDA = $_REQUEST ['BEBIDA'];
-    $IDADE = $_IDADE ['IDADE'];
+    $nome = $_REQUEST['nome'];
+    $nasc = $_REQUEST['nasc'];
+    $bebida = $_REQUEST['bebida'];
 
+    $date1 = new DateTime('now');
+    $date2 = new DateTime($nasc);
+
+    $i = $date1 -> diff($date2);
     
-    if (empty($nome)) {
+    if(empty($nome)){
+        
         $dados = array(
-        "mensagem" => 'Existe(m) campo(s) a ser(em) preenchido(s).'
+            "tipo" => 'error',
+            "mensagem" => 'Existe(m) campo(s) obrigatório(s) não preenchido(s).'
         );
+        
     } else {
 
-?>
+        if($i->y < 18) {
+            $dados = array(
+
+                "mensagem" => 'Olá, '.$nome.', sabemos que sua bebida favorita é '.$bebida.' e você não pode ingerir bebida alcoólica.'
+
+            );
+        }
+        else {
+            $dados = array(
+                "mensagem" => 'Olá, '.$nome.', sabemos que sua bebida favorita é '.$bebida.' e você pode ingerir bebida alcoólica.' 
+            );
+        }
+
+    }
+    
+
+    echo json_encode($dados);
